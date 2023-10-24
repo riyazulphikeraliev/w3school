a1860e6cf08695eb63f5626c424e6d9d85a4ddcd
RiyaZulphikerAli <riyazulphikeraliev@gmail.com>
2023-10-24 20:51:06 +0530

    First Ever Commit

---
+++ w3School/src/w3School/Main.java
@@ -0,0 +1,21 @@
+package w3School;
+public class Main  {
+
+	public static void main(String[] args) {
+		String name = "Riya Zulphiker Ali";
+		int age = 22;
+		double height = 172.01;
+		char bloodGroup = 'O';
+		String place = "Chadayamangalam";
+		int myBmi = 17;
+
+		System.out.println("My name is " + name);
+		System.out.println("My age is " + age);
+		System.out.println("I'm " + height +"cm tall");
+		System.out.println(bloodGroup);
+		System.out.println("I come from " + place);
+		System.out.println("My BMI is " + myBmi);
+
+	}
+
+}
